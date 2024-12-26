839ad80787cc77b8b3109ec7df89786bda3aabb1
Anna Taranchuk <taranchuk4@yahoo.com>
2024-12-26 12:44:52 -0500

    return from exprimental to stable next version

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Use it for PPR that is experimental now and not stable yet
+  //   experimental: {
+  //     ppr: "incremental",
+  //   },
 };
 
 export default nextConfig;
